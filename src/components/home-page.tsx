128881cd5d25366c7f6bdf334fcac6d59075a5f2
Alex Hopper <hopperdalex@gmail.com>
2025-09-07 16:01:40 -0700

    change columns responsive behavior

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -67,7 +67,7 @@ export default function HomePage({
       {/* <SignedOut> */}
       <SignedOutBanner />
       {/* </SignedOut> */}
-      <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+      <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
         <MovieColumn
           color="blue"
           status={MovieStatus.TO_WATCH}
